cf78434bf10ce27629990817b6af0b7417de08da
Izard Arthur <arthur.izard@etud.univ-montp2.fr>
2018-11-08 08:27:26 +0100

    puuuuush

--- testProjet.py
+++ testProjet.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 
 
 #getpics
-x_train= scipy.io.loadmat('/auto_home/aizard/Bureau/Cours/M2/s3/methodesSciencesDonnees/projet/Indian_pines_corrected.mat')
-x_train=x_train['indian_pines_corrected']
+bands= scipy.io.loadmat('/auto_home/aizard/Bureau/Cours/M2/s3/methodesSciencesDonnees/projet/Indian_pines_corrected.mat')
+bands=bands['indian_pines_corrected']
+
+
 
 
 #getTrainDat
@@ -13,13 +15,63 @@ trainData=np.load('/auto_home/aizard/Bureau/Cours/M2/s3/methodesSciencesDonnees/
 testData=np.load('/auto_home/aizard/Bureau/Cours/M2/s3/methodesSciencesDonnees/projet/test_data.npy')
 
 #shape
-print(x_train.shape )
-print(y_train.shape)
+print(bands.shape)
+# print(trainData.shape)
+# print(testData.shape)
+print(bands[:,:,0])
+bands=bands.astype('float32')
+
+# bands/=10000.0
+# print(bands.max())
+# print(bands.min())
+
+
+# ID 	Label
+# 2 	Maïs- Pas de Technique de Conservation des sols
+# 3 	Maïs- Minimum Tillage
+# 5 	Herbe-Pâturage
+# 6 	Herbe-Arbre
+# 10 	Soja - Pas de Technique de Conservation des sols
+# 11 	Soja - Minimum Tillage
+# 12 	Soja-clean
+# 14 	Bois
+# 15 	Bâtiment-Herbe-Arbre-drives
 
 
 
-trainData=keras.utils.to_categorical(y=trainData,num_classes=10)
-y_test=keras.utils.to_categorical(y=y_test,num_classes=10)
+#[0]
+# print(trainData[0])
+# print(testData[0])
 
-# plt.imshow(mat[:,:,50])
+cormatrix=[]
+for i in range(0,200) :
+	cor=np.corrcoef(bands[:,:,i])
+	# print(cor)
+	# print(cor.shape)
+	cormatrix=np.append(cormatrix,cor)
+
+
+print(cormatrix.shape)
+# print(len(cormatrix))
+# print(len(cormatrix[0][0]))
+
+# print(cor)
+# for i in range(0,200) :
+# 	print(bands[:,:,i].shape)
+# 	cor=np.corrcoef(bands[:,:,i],trainData,False)
+# 	print("cor")
+# 	print(cor.shape)
+
+# for pic in bands[:,:,]:
+# 	print("pic")
+# 	print(pic.shape)
+# 	print(trainData.shape)
+	# print(np.corrcoef(pic,trainData))
+
+# print(bands[:,:,0:5])
+
+# trainData=keras.utils.to_categorical(y=trainData,num_classes=10)
+# y_test=keras.utils.to_categorical(y=y_test,num_classes=10)
+# print(np.corrcoef(bands,trainData))
+# plt.imshow(bands[:,:,0])
 # plt.show()
