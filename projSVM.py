7e3b8635b0e978d6770dde555be9ff89f480b4bd
Izard Arthur <arthur.izard@etud.univ-montp2.fr>
2018-12-06 14:11:40 +0100

    svm fonctionnel mais naze

--- projSVM.py
+++ projSVM.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from time import time
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
+from sklearn import svm
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import load_files
@@ -61,7 +61,14 @@ def splitData(Y_train,Y_test, bands):
 X_train, X_test, Y_train, Y_test=splitData(Y_train, Y_test, bands)
 print(X_train.shape)
 print(X_test.shape)
-print(Y_train.shape)
-print(Y_test.shape)
 
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+svc = svm.SVC(gamma='scale')
+#n_jobs = nb of processor used. -1 = all available proc
+clf = GridSearchCV(svc, parameters,n_jobs=-1, cv=5)
+clf.fit(X_train, Y_train)  
+y_predicted=clf.predict(X_test)
 
+print(metrics.classification_report(Y_test, y_predicted))
+cm = metrics.confusion_matrix(Y_test, y_predicted)
+print(cm)   
