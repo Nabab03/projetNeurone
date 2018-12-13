3cd0b435c570e606d5feedcf15c0de11bbeb8203
Izard Arthur <arthur.izard@etud.univ-montp2.fr>
2018-12-13 15:46:09 +0100

    save newSplitY

--- split.py
+++ split.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.io as sio
 from sklearn.decomposition import PCA
 import random
+import matplotlib.pyplot as plt 
 
 def loadData():
 	bands= sio.loadmat('Indian_pines_corrected.mat')
@@ -36,25 +37,6 @@ def splitData(Y_train,Y_test, bands):
 	return np.array(trainBand), np.array(testBand), np.array(newYtrain), np.array(newYtest)
 
 
-def clusterFromBand(Y_train, Y_test, bands, cSize=3):
-	bord=cSize//2
-	trainBand=[]
-	testBand=[]
-	newYtrain=[]
-	newYtest=[]
-	ytr=0
-	yte=0
-	for i in range(bord, bands.shape[0]-(bord+1)):
-		for j in range(bord, bands.shape[1]-(bord+1)):
-			if Y_train[i][j]!=0:
-				trainBand.append(bands[i-bord:i+bord+1, j-bord:j+bord+1, :])
-				newYtrain=np.insert(newYtrain,ytr, Y_train[i][j])
-				ytr+=1
-			if Y_test[i][j]!=0:
-				testBand.append(bands[i-bord:i+bord+1, j-bord:j+bord+1, :])
-				newYtest=np.insert(newYtest,yte, Y_test[i][j])
-				yte+=1
-	return np.array(trainBand), np.array(testBand), np.array(newYtrain), np.array(newYtest)
 
 
 
@@ -119,7 +101,185 @@ def reSplitY(bands, Y_test, Y_train, ratio):
 
 	return newYTrain, newYTest
 
-	# print(bands.shape)
-	# print(Y_total.shape)
-	# X_train, X_test, Y_train, Y_test = train_test_split(bands, Y_total, test_size=0.33)
-	# return X_train, X_test, Y_train, Y_test
+
+
+
+
+
+
+
+def reSplitZoneY(bands, Y_test, Y_train, ratio,zoneSize):
+	Y_total=addMat(Y_train, Y_test)
+	newYTest=np.copy(Y_test)
+	newYTrain=np.copy(Y_train)
+	total=0
+	rayon=zoneSize//2
+
+	for i in range(Y_total.shape[0]):
+		for j in range(Y_total.shape[1]):
+			if Y_total[i,j]!=0:
+				total+=1
+
+	toPutInTrain=total*ratio
+	print(toPutInTrain)
+	for i in range(Y_total.shape[0]-(rayon+1)):
+		for j in range(Y_total.shape[1]-(rayon+1)):
+			if Y_total[i,j]!=0 and j>rayon and i>rayon and total>rayon:
+				if random.randint(1,total+1)<toPutInTrain:
+					newYTrain[i-rayon:i+rayon+1,j-rayon:j+rayon+1]=Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1]
+					newYTest[i-rayon:i+rayon+1,j-rayon:j+rayon+1]= np.zeros((zoneSize,zoneSize))
+					toPutInTrain-=Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[0] * Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[1]
+					total-=Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[0] * Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[1]
+					j+=(zoneSize*zoneSize)-1
+				else:
+					newYTest[i-rayon:i+rayon+1,j-rayon:j+rayon+1]=Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1]
+					newYTrain[i-rayon:i+rayon+1,j-rayon:j+rayon+1]= np.zeros((zoneSize,zoneSize))
+					total-=Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[0] * Y_total[i-rayon:i+rayon+1,j-rayon:j+rayon+1].shape[1]
+					j+=(zoneSize*zoneSize)-1
+			else:
+				newYTest[i,j]=0
+				newYTrain[i,j]=0
+				# print(toPutInTrain)
+				# print(total)
+		i+=(zoneSize*zoneSize) -1
+	return newYTrain, newYTest
+
+
+
+def splitY(bands, Y_train,Y_test, ratio):
+
+	Y_total=addMat(Y_train, Y_test)
+	newYTrain=np.zeros(Y_train.shape)
+	newYTest=np.zeros(Y_test.shape)
+	total=labelsNumber(Y_total)
+	toPutInTrain=total*ratio
+	print("total	toputintrain")
+	print(total)
+	print(toPutInTrain)
+	assign=np.zeros(Y_total.shape)
+	cptTrain=0
+	cptTest=0
+	i=0
+	j=0
+	print(total)
+	for i in range(Y_total.shape[0]):
+		for j in range(Y_total.shape[1]):
+			if Y_total[i,j]==0:
+				assign[i,j]=1
+			elif assign[i,j]==0:
+				label=Y_total[i,j]
+				if total>0 and random.randint(1,total+1)<toPutInTrain:
+					added,newYTrain=recursiveAddNeighbour(newYTrain, Y_total, i,j,label,0,assign)
+					toPutInTrain-=added
+					total-=added
+					cptTrain+=added
+				elif total>0:
+					added,newYTest=recursiveAddNeighbour(newYTest, Y_total, i,j,label,0,assign)
+					total-=added
+					cptTest+=added
+		
+	plt.imshow(assign)
+	plt.show()
+	return newYTrain, newYTest
+
+
+def recursiveAddNeighbour(Y, Y_T,x,y,l,addAcc, assign):
+	added=addAcc
+	if Y_T[x,y]==l and assign[x,y]==0:
+		assign[x,y]=1
+		added+=1
+		Y[x,y]=Y_T[x,y]
+
+	#si case de gauche du même label
+	tmp=0
+	if x-1>0 and Y_T[x-1,y]==l and assign[x-1,y]==0:
+		tmp,Y=recursiveAddNeighbour(Y,Y_T,x-1,y,l,added,assign)
+		added+=tmp
+	elif x+1<Y_T.shape[0] and Y_T[x+1,y]==l and assign[x+1,y]==0:
+		tmp,Y=recursiveAddNeighbour(Y,Y_T,x+1,y,l,added,assign)
+		added+=tmp
+	elif y-1>0 and Y_T[x,y-1]==l and assign[x,y-1]==0:
+		tmp,Y=recursiveAddNeighbour(Y,Y_T,x,y-1,l,added,assign)
+		added+=tmp
+	elif y+1<Y_T.shape[1] and Y_T[x,y+1]==l and assign[x,y+1]==0:
+		tmp,Y=recursiveAddNeighbour(Y,Y_T,x,y+1,l,added,assign)
+		added+=tmp
+
+	return added,Y
+	
+
+
+
+
+
+
+def rec(Y, Y_T,x,y,l,addAcc, assign):
+	addAcc=addAcc
+
+	#si case de gauche du même label
+
+	if x-1>0 and Y_T[x-1,y]==l and assign[x-1,y]==0:
+		Y[x-1,y]=Y_T[x-1,y]
+		assign[x-1,y]=1
+		addAcc+=1+recursiveAddNeighbour(Y,Y_T,x-1,y,l,addAcc,assign)
+	elif x+1<Y_T.shape[0] and Y_T[x+1,y]==l and assign[x+1,y]==0:
+		Y[x+1,y]=Y_T[x+1,y]
+		assign[x+1,y]=1
+		addAcc+=1+recursiveAddNeighbour(Y,Y_T,x+1,y,l,addAcc,assign)
+	elif y-1>0 and Y_T[x,y-1]==l and assign[x,y-1]==0:
+		Y[x,y-1]=Y_T[x,y-1]
+		assign[x,y-1]=1
+		addAcc+=1+recursiveAddNeighbour(Y,Y_T,x,y-1,l,addAcc,assign)
+	elif y+1<Y_T.shape[1] and Y_T[x,y+1]==l and assign[x,y+1]==0:
+		Y[x,y+1]=Y_T[x,y+1]
+		assign[x,y+1]=1
+		addAcc+=1+recursiveAddNeighbour(Y,Y_T,x,y+1,l,addAcc,assign)
+	return addAcc
+		
+
+
+
+
+
+
+
+
+def labelsNumber(Y):
+	total=0
+	for i in range(Y.shape[0]):
+		for j in range(Y.shape[1]):
+			if Y[i,j]!=0:
+				total+=1
+	return total
+#trainBand.append(bands[i-bord:i+bord+1, j-bord:j+bord+1, :])
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+
+
+
+
+
+
+def clusterFromBand(Y_train, Y_test, bands, cSize=3):
+	bord=cSize//2
+	trainBand=[]
+	testBand=[]
+	newYtrain=[]
+	newYtest=[]
+	ytr=0
+	yte=0
+	for i in range(bord, bands.shape[0]-(bord+1)):
+		for j in range(bord, bands.shape[1]-(bord+1)):
+			if Y_train[i][j]!=0:
+				trainBand.append(bands[i-bord:i+bord+1, j-bord:j+bord+1, :])
+				newYtrain=np.insert(newYtrain,ytr, Y_train[i][j])
+				ytr+=1
+			if Y_test[i][j]!=0:
+				testBand.append(bands[i-bord:i+bord+1, j-bord:j+bord+1, :])
+				newYtest=np.insert(newYtest,yte, Y_test[i][j])
+				yte+=1
+	return np.array(trainBand), np.array(testBand), np.array(newYtrain), np.array(newYtest)
---
+++ test.py
@@ -0,0 +1,40 @@
+from split import clusterFromBand,splitY,reSplitY, addMat, loadData,removeUselessLabel, reSplitZoneY
+import matplotlib.pyplot as plt 
+
+bands, Y_train, Y_test=loadData()
+removeUselessLabel(Y_test,Y_train)
+
+
+
+Y_train, Y_test= splitY(bands, Y_train,Y_test, 0.8)
+
+
+# print(X_train.shape)
+# print(X_test.shape)
+# print(Y_train[0])
+# print(Y_test[0])
+Y_total=addMat(Y_train, Y_test)
+
+f, (ax1, ax2, ax3) = plt.subplots(1,3, sharey=True)
+ax1.imshow(Y_train)
+ax2.imshow(Y_test)
+ax3.imshow(Y_total)
+
+plt.show()
+
+
+# X_train, X_test, Y_train, Y_test=clusterFromBand(Y_train, Y_test, bands,5)
+
+# print(X_train.shape)
+# print(X_test.shape)
+
+# nsamples, nx, ny, nw = X_train.shape
+# X_train = X_train.reshape((nsamples,nx*ny*nw))
+
+# nsamples, nx, ny,nw = X_test.shape
+# X_test = X_test.reshape((nsamples,nx*ny*nw))
+
+
+# print(X_train.shape)
+# print(X_test.shape)
+
